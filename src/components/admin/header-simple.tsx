828cc65a14567b43d5c246e3e24daeee120beb27
Jesús David Aguilar Lopez <jesusaguilop432@gmail.com>
2025-10-13 21:57:18 +0000

    En la vista de Movil al iniciar sesión no se como acceder a el panel de

--- src/components/admin/header-simple.tsx
+++ src/components/admin/header-simple.tsx
@@ -1,7 +1,16 @@
+'use client';
+
 import Link from 'next/link';
 import { MainNavSimple } from '@/components/admin/main-nav-simple';
 import { UserNav } from '@/components/admin/user-nav';
-import { Trees } from 'lucide-react';
+import { Menu, Trees } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+} from '@/components/ui/sheet';
+import { Button } from '../ui/button';
 
 export default function HeaderSimple() {
   return (
@@ -16,7 +25,37 @@ export default function HeaderSimple() {
           <span className="font-bold hidden sm:inline-block">VerdeQR Admin</span>
         </Link>
         <MainNavSimple className="hidden md:flex" />
-        <div className="ml-auto flex items-center space-x-4">
+
+        <div className="ml-auto flex items-center space-x-2">
+          <div className="md:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Menu className="h-6 w-6" />
+                  <span className="sr-only">Abrir menú</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left">
+                <nav className="grid gap-6 text-lg font-medium p-4">
+                  <Link
+                    href="#"
+                    className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                  >
+                    <Trees className="h-5 w-5 transition-all group-hover:scale-110" />
+                    <span className="sr-only">VerdeQR</span>
+                  </Link>
+                  <SheetClose asChild>
+                    <Link
+                      href="/admin/management"
+                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                    >
+                      Gestión
+                    </Link>
+                  </SheetClose>
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </div>
           <UserNav />
         </div>
       </div>
