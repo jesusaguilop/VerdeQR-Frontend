af7ca227901b2d1b43993aa276d5b0a030a43d89
Jesús David Aguilar Lopez <jesusaguilop432@gmail.com>
2025-10-14 15:08:26 +0000

    No veo la opción de instalar o es solo para el cel?

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
   fallbacks: {
     document: '/offline',
   }
